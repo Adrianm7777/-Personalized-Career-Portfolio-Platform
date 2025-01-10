0aac46bea426c373ecca839301f1174d99e16443
Adrianm7777 <Adrianmaterka10@gmail.com>
2025-01-10 17:43:34 +0100

    feature: Add CORS support for frontend integration

--- personalized_portfolio_backend/personalized_portfolio_backend/settings.py
+++ personalized_portfolio_backend/personalized_portfolio_backend/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'api',
     'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,3 +131,8 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+]
