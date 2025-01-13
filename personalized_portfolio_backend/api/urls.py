49432eeae85ae381f0ad16c51c206064991fd699
Adrianm7777 <Adrianmaterka10@gmail.com>
2025-01-13 17:18:18 +0100

    feature: Add login and logout endpoints to API

--- personalized_portfolio_backend/api/urls.py
+++ personalized_portfolio_backend/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import portfolio_data
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import portfolio_data ,login_view ,logout_view
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("portfolio/", portfolio_data, name="portfolio-data"),
-    path("token/",TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("login/",login_view, name="login"),
+    path("logout/", logout_view, name ="logout"),
     path("token/refresh/",TokenRefreshView.as_view(),name="token_refresh")
 ]
