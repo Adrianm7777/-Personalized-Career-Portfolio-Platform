2fe63a3a4103398cc7e1a2c9417cb3b788fab9c4
Adrianm7777 <Adrianmaterka10@gmail.com>
2025-01-15 17:53:46 +0100

    refactor: Remove unused loginHandler function from API utility

--- personalized-portfolio-frontend/utils/api.ts
+++ personalized-portfolio-frontend/utils/api.ts
@@ -1,5 +1,4 @@
 import { IPortfolioData } from "@/types/types";
-import { NextApiRequest, NextApiResponse } from "next";
 
 export const fetchPortfolios = async (
   search: string = "",
@@ -16,36 +15,4 @@ export const fetchPortfolios = async (
   }
   const data = await response.json();
   return data.results || [];
-};
-
-export const loginHandler = async (
-  req: NextApiRequest,
-  res: NextApiResponse
-) => {
-  if (req.method !== "POST") {
-    return res.status(405).json({ message: "Method not allowed" });
-  }
-
-  const { username, password } = req.body;
-
-  try {
-    const response = await fetch("http://127.0.0.1:8000/api/token/", {
-      method: "POST",
-      headers: { "Content-type": "application/json" },
-      body: JSON.stringify({ username, password }),
-    });
-
-    if (!response.ok) {
-      const errorData = await response.json();
-      return res.status(response.status).json({ errorData });
-    }
-
-    const data = await response.json();
-    return res.status(200).json(data);
-  } catch (error) {
-    const errorMessage =
-      error instanceof Error ? error.message : "Unknown error occurred";
-    console.error("Error during login:", errorMessage);
-    return res.status(500).json({ error: "Internal server error." });
-  }
 };
