fe167ac503b22d5f90bbe85b36aca96553120924
Adrianm7777 <Adrianmaterka10@gmail.com>
2025-01-16 17:19:50 +0100

    feature: Implement loginHandler utility for handling user authentication

---
+++ personalized-portfolio-frontend/utils/login.ts
@@ -0,0 +1,22 @@
+export const loginHandler = async (username: string, password: string) => {
+  try {
+    const response = await fetch("http://127.0.0.1:8000/api/login/", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ username, password }),
+      credentials: "include",
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.message || "Login failed");
+    }
+    const data = await response.json();
+    return { success: true, data };
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error ? error.message : "Unknown error occurred";
+    console.error("Error during login:", errorMessage);
+    return { success: false, error: errorMessage };
+  }
+};
