b41e407629a01f63a07c1a91a5fb07c22ccb65f7
Adrianm7777 <Adrianmaterka10@gmail.com>
2025-01-20 17:40:28 +0100

    feature: Add sign-up endpoint to API for user registration

--- personalized_portfolio_backend/api/urls.py
+++ personalized_portfolio_backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import portfolio_data ,login_view ,logout_view
+from .views import portfolio_data ,login_view ,logout_view, sign_up
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("portfolio/", portfolio_data, name="portfolio-data"),
     path("login/",login_view, name="login"),
     path("logout/", logout_view, name ="logout"),
-    path("token/refresh/",TokenRefreshView.as_view(),name="token_refresh")
+    path("token/refresh/",TokenRefreshView.as_view(),name="token_refresh"),
+    path("signup/",sign_up, name="sign-up")
 ]
